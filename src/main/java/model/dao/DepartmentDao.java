661dfe53420062aebc851efcc828f3b96c491d90
Luiz Diego <luiz.1324diego@gmail.com>
2026-09-15 21:08:22 -0300

    SellerDao and DepartmentDao

---
+++ src/main/java/model/dao/DepartmentDao.java
@@ -0,0 +1,16 @@
+package model.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entities.Department;
+
+public interface DepartmentDao {
+
+    public void insert(Department obj);
+    public void update(Department obj);
+    public void deletById(Integer id);
+    public Department findById(Integer id);
+    public List<Department> findAll = new ArrayList<>();
+
+}
