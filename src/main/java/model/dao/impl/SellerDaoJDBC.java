f354a062a205e18857cc9d3d3e4ea115a7827d9b
Luiz Diego <luiz.1324diego@gmail.com>
2026-09-15 21:30:35 -0300

    DaoFactory and SellerDaoJDBC

---
+++ src/main/java/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,32 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao{
+
+    @Override
+    public void insert(Seller obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    }
+
+    @Override
+    public void update(Seller obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void deletById(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deletById'");
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+}
